e2314ceace701ddaa8a59e85618a77e6afd2f547
Richard Neomy <richard.neomy@teknik.io>
2021-04-07 04:57:02 -0400

    fixes #220

--- background.js
+++ background.js
@@ -41,12 +41,17 @@ const starters = {
     starters.cache.push(c);
   }
 };
-storage(prefs).then(ps => {
-  Object.assign(prefs, ps);
-  starters.ready = true;
-  starters.cache.forEach(c => c());
-  delete starters.cache;
-});
+{
+  // preference are only up-to-date on the first run. For all other needs call storage().then()
+  const once = storage(prefs).then(ps => {
+    Object.assign(prefs, ps);
+    starters.ready = true;
+    starters.cache.forEach(c => c());
+    delete starters.cache;
+  });
+  chrome.runtime.onStartup.addListener(once);
+  chrome.runtime.onInstalled.addListener(once);
+}
 
 // clear session only hostnames from the exception list; only on the local machine
 starters.push(() => chrome.storage.local.set({
