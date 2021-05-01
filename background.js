625e1f844185692d5804a2153d3798ce7aae577e
Richard Neomy <richard.neomy@teknik.io>
2021-05-01 02:11:42 -0400

    cleanup

--- background.js
+++ background.js
@@ -43,14 +43,12 @@ const starters = {
 
 {
   // preference are only up-to-date on the first run. For all other needs call storage().then()
-  const once = () => {
-    storage(prefs).then(ps => {
-      Object.assign(prefs, ps);
-      starters.ready = true;
-      starters.cache.forEach(c => c());
-      delete starters.cache;
-    });
-  };
+  const once = () => storage(prefs).then(ps => {
+    Object.assign(prefs, ps);
+    starters.ready = true;
+    starters.cache.forEach(c => c());
+    delete starters.cache;
+  });
   chrome.runtime.onStartup.addListener(once);
   chrome.runtime.onInstalled.addListener(once);
 }
