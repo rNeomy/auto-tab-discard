e7e51ce6c9c9a5e50b80fcb0909a2d03b4d38a26
Richard Neomy <richard.neomy@teknik.io>
2021-12-12 01:39:13 -0500

    improving lazy discarding; based on #251

--- data/lazy.js
+++ data/lazy.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const s = document.title;
+
 document.open();
 document.close();
 const title = document.createElement('title');
+title.textContent = s || location.href;
 if (!document.head) {
   const head = document.createElement('head');
   document.documentElement.appendChild(head);
