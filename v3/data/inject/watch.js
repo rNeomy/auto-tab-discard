de5b55fba56f0b73543f8f95e41eebce0b74b8b7
Richard Neomy <richard.neomy@teknik.io>
2022-10-15 03:49:33 -0400

    improves form detection; based on #312

--- v3/data/inject/watch.js
+++ v3/data/inject/watch.js
@@ -1,31 +1,56 @@
 /* this watches if there are any unsaved forms on the page */
 
-window.isReceivingFormInput = false;
+let checked = false;
+const elements = new Set();
+
+Object.defineProperty(window, 'isReceivingFormInput', {
+  get() {
+    // there is no attached modified element or all of them are empty
+    try {
+      if ([...elements].filter(e => e.isConnected && (e.value || e.textContent)).length === 0) {
+        return false;
+      }
+    }
+    catch (e) {}
+
+    return checked;
+  }
+});
+// reset on submit;
+addEventListener('submit', () => {
+  checked = false;
+  elements.clear();
+});
+
 addEventListener('keydown', e => {
   const {keyCode, target, path} = e;
   // check target
   if (keyCode >= 48 && keyCode <= 90 && target.tagName) {
     if (target.isContentEditable) {
-      window.isReceivingFormInput = true;
+      elements.add(target);
+      checked = true;
     }
     if (target.tagName === 'INPUT' || target.tagName === 'TEXTAREA' || target.tagName === 'FORM') {
-      window.isReceivingFormInput = true;
+      elements.add(target);
+      checked = true;
     }
     if (target.type === 'application/pdf') {
-      window.isReceivingFormInput = true;
+      checked = true;
     }
   }
   // check custom elements
   if (keyCode >= 48 && keyCode <= 90 && path && path[0] !== target) {
     const o = path[0];
     if (o.isContentEditable) {
-      window.isReceivingFormInput = true;
+      elements.add(o);
+      checked = true;
     }
     if (o.tagName === 'INPUT' || o.tagName === 'TEXTAREA' || o.tagName === 'FORM') {
-      window.isReceivingFormInput = true;
+      elements.add(o);
+      checked = true;
     }
     if (o.type === 'application/pdf') {
-      window.isReceivingFormInput = true;
+      checked = true;
     }
   }
 }, true);
