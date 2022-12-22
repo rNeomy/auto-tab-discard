6794ef9b413a84c071198616538fd5486a64472f
Richard Neomy <richard.neomy@teknik.io>
2022-12-22 05:48:58 -0500

    version 0.6.7.1

--- v3/worker/core/startup.mjs
+++ v3/worker/core/startup.mjs
@@ -15,9 +15,10 @@ const starters = {
   // preference are only up-to-date on the first run. For all other needs call storage().then()
   const once = () => storage(prefs).then(ps => {
     Object.assign(prefs, ps);
+
     starters.ready = true;
     starters.cache.forEach(c => c());
-    delete starters.cache;
+    starters.cache.length = 0;
   });
 
   chrome.runtime.onStartup.addListener(once);
